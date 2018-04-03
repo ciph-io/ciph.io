9faaa76df6cee033b65d4fc4dd1654044827553d
Ersun Warncke <ersun.warncke@gmail.com>
2018-04-03 13:20:10 -0400

    remove console log

--- node/lib/upload-service.js
+++ node/lib/upload-service.js
@@ -16,7 +16,6 @@ const blockSizes = BlockService.getBlockSizes()
 module.exports = class UploadService {
 
     static async processUpload (args, file) {
-        console.log("XXX", process.env.SERVER_ID, process.env.PORT)
         try {
             // validate signature
             const requestSignature = ServerService.getServerSignature(args.size+args.blockId)
